60df22466e0c6a02e4484ce73da6fc87207ffcba
shovity <shovity@gmail.com>
2023-06-28 10:15:42 +0700

    init

---
+++ app.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const fs = require('node:fs/promises')
+const readline = require('node:readline')
+const io = require('socket.io-client')
+
+const channel = process.argv[2]
+const guser = process.argv[3]
+
+const socket = io('http://103.147.34.87:8000', {
+    transports: ['websocket'],
+    path: '/holaa/socket.io',
+})
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+const prompt = async (label, falback) => {
+    const response = (await new Promise((resolve) => rl.question(label, resolve))) || falback
+
+    if (response === undefined) {
+        return await prompt(label)
+    }
+
+    return response
+}
+
+const getUser = async () => {
+    try {
+        return guser || (await fs.readFile('user')).toString()
+    } catch (error) {
+        const user = await prompt('Enter username> ')
+        fs.writeFile('user', user)
+        return user
+    }
+}
+
+const main = async () => {
+    const user = await getUser()
+
+    socket.emit('join', { user, channel })
+
+    socket.on('message', (data) => {
+        for (const { user, content } of data) {
+            console.log(`${user}> ${content}`)
+        }
+    })
+
+    while (true) {
+        const content = await prompt(`${user}> `)
+        socket.emit('message', { content })
+    }
+}
+
+main().catch(console.error)
